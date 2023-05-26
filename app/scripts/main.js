0f6c60e9ba2df6acd073c5236b524f8a0ef15faf
DanilNefedov <ndanil2@ukr.net>
2023-05-26 19:35:41 +0300

    header swiper | benefits | about our sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,19 +24,16 @@ document.addEventListener('click', e => {
 })
 
 
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     loop: true,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 16,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 
 
